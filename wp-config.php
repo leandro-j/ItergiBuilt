ae4e029adece3f87ba4086f2934c97c0ee5ee7fe
Valentine Moroz <uaeconomclass@gmail.com>
2018-11-02 12:41:01 +0530

    changes in wp-config

--- wp-config.php
+++ wp-config.php
@@ -20,7 +20,7 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define('DB_NAME', 'uaecono7_d14707u');
+define('DB_NAME', 'uaecono7_d14707u'); 
 
 /** MySQL database username */
 define('DB_USER', 'uaecono7_d14707u');
