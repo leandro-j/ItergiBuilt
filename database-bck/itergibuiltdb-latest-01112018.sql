23db269fb4b583ac89b463d9525b4a56f3beabbc
Valentine Moroz <uaeconomclass@gmail.com>
2018-11-02 12:27:25 +0530

    Inserted some files for server

--- database-bck/itergibuiltdb-latest-01112018.sql
+++ database-bck/itergibuiltdb-latest-01112018.sql
@@ -1,31 +1,3 @@
--- phpMyAdmin SQL Dump
--- version 4.4.15.9
--- https://www.phpmyadmin.net
---
--- Host: localhost
--- Generation Time: Nov 01, 2018 at 02:14 PM
--- Server version: 5.6.35
--- PHP Version: 5.3.29
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Database: `itergibuiltdb`
---
-
--- --------------------------------------------------------
-
---
--- Table structure for table `itergibuilt_wp_commentmeta`
---
-
 CREATE TABLE IF NOT EXISTS `itergibuilt_wp_commentmeta` (
   `meta_id` bigint(20) unsigned NOT NULL,
   `comment_id` bigint(20) unsigned NOT NULL DEFAULT '0',
@@ -887,6 +859,3 @@ ALTER TABLE `itergibuilt_wp_usermeta`
 --
 ALTER TABLE `itergibuilt_wp_users`
   MODIFY `ID` bigint(20) unsigned NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=2;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
--- wp-config.php
+++ wp-config.php
@@ -20,13 +20,13 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define('DB_NAME', 'itergibuiltdb');
+define('DB_NAME', 'uaecono7_d14707u');
 
 /** MySQL database username */
-define('DB_USER', 'root');
+define('DB_USER', 'uaecono7_d14707u');
 
 /** MySQL database password */
-define('DB_PASSWORD', 'mysql');
+define('DB_PASSWORD', 'ctSK4*LL');
 
 /** MySQL hostname */
 define('DB_HOST', 'localhost');
