1381ca4a7bafcc4639e5e47db47066008f64bf59
Valentine Moroz <uaeconomclass@gmail.com>
2018-11-08 15:36:16 +0530

    Task-13: IntegriWarrantyTM – it shows up a couple of times on the page, it should be IntegriWarrantyTM.  Made Changes in wp-content/theme/Divi-child/css/custom.css

--- wp-content/themes/Divi-child/functions.php
+++ wp-content/themes/Divi-child/functions.php
@@ -1,19 +1,16 @@
 <?php
-//Connected style files .css and .js files to the current (child) theme Divi-child
 add_action( 'wp_enqueue_scripts', 'my_theme_enqueue_styles' );
 function my_theme_enqueue_styles() {
-	//styles
     wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
 	wp_enqueue_style( 'custom_css', get_stylesheet_directory_uri() . '/css/custom.css' );
 	wp_enqueue_style( 'custom_css2', get_stylesheet_directory_uri() . '/css/custom2.css' );
 	wp_enqueue_style( 'owl_css', get_stylesheet_directory_uri() . '/css/owl.css' );
 	wp_enqueue_style( 'owl_theme_css', get_stylesheet_directory_uri() . '/css/owl.theme.default.css' );
-	//.js files. owl carousel and custom js
 	wp_enqueue_script( 'owl_js', get_stylesheet_directory_uri() . '/js/owl.carousel.min.js',false);
 	wp_enqueue_script( 'custom-js', get_stylesheet_directory_uri() . '/js/custom.js?'.time(),false);
 }
 
-//Created a custom sidebar. Sidebar name is "Footer Bottom. Added styles for this widget and for heading.
+
 function my_custom_sidebar() {
     register_sidebar(
         array (
@@ -29,13 +26,12 @@ function my_custom_sidebar() {
 }
 add_action( 'widgets_init', 'my_custom_sidebar' );
 
-//added setting for theme customizer. Most of them with default values.
  function custom_register_theme_customizer( $wp_customize ) {
  
     $wp_customize->add_section('home_page_banner_section', array(
 		'title'          => 'Home Page Banner Section'
 	));
-		//added setting for footer text area
+		//adding setting for footer text area
 	$wp_customize->add_setting('sk_home_top_slider_image1',array(
 			'default'		=> 'wp-content/uploads/2018/11/pic1.png',
 			'sanitize_callback'	=> 'esc_url_raw',
@@ -86,7 +82,6 @@ add_action( 'widgets_init', 'my_custom_sidebar' );
 }
 add_action( 'customize_register', 'custom_register_theme_customizer' );
 
-//Created custom post type "work". Capability type is post. Not hierarchical. Has archive.
 function work_custom_post_type(){
 	$args = array(
 		'label'=>'Our Work',
@@ -115,7 +110,6 @@ function work_custom_post_type(){
 }
 add_action('init','work_custom_post_type',0);
 
-//Created custom category for post type "work". Hierarchical. Slug is "our-work".
 function work_custom_category() {
   $args = array(
     'label' => 'Categories',
@@ -127,43 +121,13 @@ function work_custom_category() {
 }
 add_action( 'init', 'work_custom_category');
 
-//1.Created divi faq accordeon toggle functionality 2. Created sticky menu functionality when scrolling.
-add_action('wp_footer','faq_toggle_closed_default');
-function faq_toggle_closed_default(){?>
-	<script>
-	//1.Created divi faq accordeon toggle functionality 
-	jQuery(function($){
-		$('.et_pb_accordion .et_pb_toggle_open').addClass('et_pb_toggle_close').removeClass('et_pb_toggle_open');
 
-		$('.et_pb_accordion .et_pb_toggle').click(function() {
-			$this = $(this);
-			setTimeout(function(){
-				$this.closest('.et_pb_accordion').removeClass('et_pb_accordion_toggling');
-			},700);
-		});	
-	});
-	//2. Created sticky menu functionality when scrolling.
-	jQuery(window).load(function(){
-		var distance = jQuery('.scroll_sticky,.main-menu').offset().top,
-		$window = jQuery(window);
-		jQuery(window).scroll(function(){			
-			if ( $window.scrollTop() >= distance  ) {
-				jQuery('.scroll_sticky,.main-menu').addClass('fixed-header');
-			} else {
-				jQuery('.scroll_sticky,.main-menu').removeClass('fixed-header');
-			}			
-		});
-	});
-	</script>
-<?php }
-
-//Added one image size with cropping. 480px*350px. Name of the image size is "work-cropped-480x350"
 add_action('init','image_resize_custom');
 function image_resize_custom(){
 	add_image_size( 'work-cropped-480x350', 480, 350, true );
 }
 
-//Created Contact Form Date Slider. It's an owl carousel. Described there: https://owlcarousel2.github.io/OwlCarousel2/
+/*******Contact Form Date Slider*********/
 function contact_date_shortcode(){
 	ob_start();
 	?>
@@ -195,5 +159,4 @@ function contact_date_shortcode(){
 	<?Php
 	return ob_get_clean();
 }
-//Created a shortcode for this slider [contact_date_shortcode]
 add_shortcode('contact_date_shortcode','contact_date_shortcode');
