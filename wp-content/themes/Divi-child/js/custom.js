1ec3108c35e9546da0d77a49f71b64717efe7929
Valentine Moroz <uaeconomclass@gmail.com>
2018-11-08 15:09:52 +0530

    Task-11: The FAQ’s – there is no way to collapse them once they’re opened – clicking on the title should collapse them.  Made Changes in wp-content/theme/Divi-child/js/custom.js

--- wp-content/themes/Divi-child/js/custom.js
+++ wp-content/themes/Divi-child/js/custom.js
@@ -49,11 +49,40 @@ jQuery(document).ready(function() {
 		jQuery(this).toggleClass('popped');
     });
 	
+
+	jQuery('.et_pb_accordion .et_pb_toggle_open').addClass('et_pb_toggle_close').removeClass('et_pb_toggle_open');
+		jQuery('.et_pb_toggle_title').click(function(){
+			var $toggle = jQuery(this).closest('.et_pb_toggle');
+			if (!$toggle.hasClass('et_pb_accordion_toggling')) {
+				var $accordion = $toggle.closest('.et_pb_accordion');
+				if ($toggle.hasClass('et_pb_toggle_open')) {
+					$accordion.addClass('et_pb_accordion_toggling');
+					$toggle.find('.et_pb_toggle_content').slideToggle(700, function() { 
+					$toggle.removeClass('et_pb_toggle_open').addClass('et_pb_toggle_close'); 
+				});
+			}
+			setTimeout(function(){ 
+				$accordion.removeClass('et_pb_accordion_toggling'); 
+			}, 750);
+		}
+	});
+	
+
 });
 
 jQuery(window).load(function(){
 	jQuery("body").find('li.menu-item-has-children > a').after('<span class="sub-menu-toggle"></span>');
 	
+	
+	var distance = jQuery('.scroll_sticky,.main-menu').offset().top,
+	$window = jQuery(window);
+	jQuery(window).scroll(function(){			
+		if ( $window.scrollTop() >= distance  ) {
+			jQuery('.scroll_sticky,.main-menu').addClass('fixed-header');
+		} else {
+			jQuery('.scroll_sticky,.main-menu').removeClass('fixed-header');
+		}			
+	});
 	//jQuery("body #main-header .mobile_nav #mobile_menu li.menu-item-has-children > a").next().append('<span class="arrow"><i></i></span>');
    // jQuery("body #main-header .mobile_nav #mobile_menu > li.menu-item-has-children > .sub-menu > .menu-item-has-children > a").next().append('<span class="arrow"><i class="fa fa-chevron-down"></i></span>');
 });
